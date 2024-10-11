d699014cd9c4ce6be9114c79e45b4d3ece0f9250
akv3sic <57301167+akv3sic@users.noreply.github.com>
2024-10-11 19:41:42 +0200

    Add StaffService

---
+++ FireSync/Services/Interfaces/IStaffService.cs
@@ -0,0 +1,17 @@
+﻿using FireSync.DTOs;
+
+namespace FireSync.Services.Interfaces
+{
+    public interface IStaffService
+    {
+        /// <summary>
+        /// Gets all staff members.
+        /// </summary>
+        Task<List<UserOutputDto>> GetStaffAsync();
+
+        /// <summary>
+        /// Gets all firefighters.
+        /// </summary>
+        Task<List<UserOutputDto>> GetFirefightersAsync();
+    }
+}
---
+++ FireSync/Services/StaffService.cs
@@ -0,0 +1,76 @@
+﻿using FireSync.DTOs;
+using FireSync.Enums;
+using FireSync.Models;
+using FireSync.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FireSync.Services
+{
+    public class StaffService : IStaffService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public StaffService(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        /// <inheritdoc/>
+        public async Task<List<UserOutputDto>> GetStaffAsync()
+        {
+            var users = await this.userManager.Users.ToListAsync();
+            var userOutputDtos = new List<UserOutputDto>();
+
+            foreach (var user in users)
+            {
+                var roles = await this.userManager.GetRolesAsync(user);
+                var userOutputDto = new UserOutputDto
+                {
+                    Id = Guid.Parse(user.Id),
+                    Username = user.UserName ?? string.Empty,
+                    Email = user.Email ?? string.Empty,
+                    Roles = roles.ToList(),
+                    FirstName = user.FirstName ?? string.Empty,
+                    LastName = user.LastName ?? string.Empty,
+                };
+
+                userOutputDtos.Add(userOutputDto);
+            }
+
+            return userOutputDtos;
+        }
+
+        /// <inheritdoc/>
+        public async Task<List<UserOutputDto>> GetFirefightersAsync()
+        {
+            var users = await this.userManager.Users.ToListAsync();
+            var userOutputDtos = new List<UserOutputDto>();
+
+            foreach (var user in users)
+            {
+                var roles = await this.userManager.GetRolesAsync(user);
+
+                if (!roles.Contains(Roles.Firefighter.ToString()))
+                {
+                    continue;
+                }
+
+                var userOutputDto = new UserOutputDto
+                {
+                    Id = Guid.Parse(user.Id),
+                    Username = user.UserName ?? string.Empty,
+                    Email = user.Email ?? string.Empty,
+                    Roles = roles.ToList(),
+                    FirstName = user.FirstName ?? string.Empty,
+                    LastName = user.LastName ?? string.Empty,
+                };
+
+                userOutputDtos.Add(userOutputDto);
+            }
+
+            return userOutputDtos;
+        }
+    }
+}
