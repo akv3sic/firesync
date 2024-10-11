ed38d87576cd4425831eae39a0a43a2cd46ebf5d
akv3sic <57301167+akv3sic@users.noreply.github.com>
2024-10-11 13:13:48 +0200

    Extend ApplicationUser and add Roles enum #8

--- FireSync/Entities/ApplicationUser.cs
+++ FireSync/Entities/ApplicationUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace FireSync.Models
 {
@@ -7,5 +8,30 @@ namespace FireSync.Models
     /// </summary>
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// Gets or sets the first name of the application user.
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the last name of the application user.
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the date of birth of the application user.
+        /// </summary>
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the application user.
+        /// </summary>
+        [MaxLength(200)]
+        public string Address { get; set; } = string.Empty;
     }
 }
---
+++ FireSync/Enums/Roles.cs
@@ -0,0 +1,14 @@
+﻿namespace FireSync.Enums
+{
+    /// <summary>
+    /// Represents the roles.
+    /// </summary>
+    public enum Roles
+    {
+        Admin,
+        Firefighter,
+        Dispatcher,
+        Accountant,
+        LegalOfficer
+    }
+}
