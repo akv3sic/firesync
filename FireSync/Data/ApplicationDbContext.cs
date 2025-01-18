0075a34cf606dfe9524b05162bbfb7049209c70c
akv3sic <57301167+akv3sic@users.noreply.github.com>
2025-01-18 23:15:53 +0100

    Implement soft deletion mechanism in ApplicationDbContext

--- FireSync/Data/ApplicationDbContext.cs
+++ FireSync/Data/ApplicationDbContext.cs
@@ -2,6 +2,8 @@
 using FireSync.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using FireSync.Data.Extensions;
+using FireSync.Entities.Common.Interfaces;
 
 namespace FireSync.Data
 {
@@ -29,6 +31,27 @@ namespace FireSync.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.ApplyGlobalFilters();
+        }
+
+        /// <summary>
+        /// Saves the changes to the database.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The number of state entries written to the database.</returns>
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in this.ChangeTracker.Entries<IDeletableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.Entity.DeletedAt = DateTime.UtcNow;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ FireSync/Data/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿using FireSync.Entities.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FireSync.Data.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="ModelBuilder"/> to apply global filters.
+    /// </summary>
+    public static class ModelBuilderExtensions
+    {
+        private static readonly MethodInfo SetGlobalQueryMethod = typeof(ModelBuilderExtensions).GetMethod(nameof(SetGlobalQuery), BindingFlags.NonPublic | BindingFlags.Static)
+                                                                                     ?? throw new MissingMethodException(nameof(ModelBuilderExtensions), nameof(SetGlobalQuery));
+
+        /// <summary>
+        /// Applies global filters to entities.
+        /// </summary>
+        /// <param name="builder">The <see cref="ModelBuilder"/>.</param>
+        public static void ApplyGlobalFilters(this ModelBuilder builder)
+        {
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                if (typeof(IDeletableEntity).IsAssignableFrom(entityType.ClrType))
+                {
+                    var method = SetGlobalQueryMethod.MakeGenericMethod(entityType.ClrType);
+                    method.Invoke(null, new object[] { builder });
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets a global query filter for soft deleting entities of type <typeparamref name="T"/>.
+        /// Entities are considered soft deleted if their <c>DeletedAt</c> property is not null.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity to which the filter will be applied.</typeparam>
+        /// <param name="builder">The <see cref="ModelBuilder"/>.</param>
+        private static void SetGlobalQuery<T>(ModelBuilder builder)
+            where T : class, IDeletableEntity
+        {
+            builder.Entity<T>().HasQueryFilter(e => EF.Property<DateTime?>(e, "DeletedAt") == null);
+        }
+    }
+}
