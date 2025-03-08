fc8740caf0171fc47a4a0affcb41d0ed3e6cf984
akv3sic <57301167+akv3sic@users.noreply.github.com>
2025-03-08 21:08:46 +0100

    Add N/A placeholder for empty intervention type descriptions

---
+++ FireSync/Constants/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace FireSync.Constants
+{
+    /// <summary>
+    /// Contains application-wide constants.
+    /// </summary>
+    public static class GlobalConstants
+    {
+        /// <summary>
+        /// The not applicable value.
+        /// </summary>
+        public const string NotApplicable = "N/A";
+    }
+}
